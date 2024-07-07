7fdd33c34e41cf0a06db04a05ea557bb1f19ee8e
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2024-07-07 22:58:34 +0530

    test

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def main():
             data = zlib.decompress(data)
             # skip till \x00
             data = data[data.index(b'\x00')+1:]
-            print(data)
+            dprint(data.decode())
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
